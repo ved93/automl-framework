83f14d01be70a0f06f792fa5bd8619c4eb2a59fa
ved93 <vpved93@gmail.com>
2020-05-19 19:15:51 +0530

    Visualization : added data audit

--- src/visualize.py
+++ src/visualize.py
@@ -1,2 +1,19 @@
 
 import pandas as pd
+import pandas_profiling 
+
+# TRAINING_DATA = os.environ.get("TRAINING_DATA")
+
+    
+def create_summary_report(df):
+    #Data profiling/EDA 
+    profile = pandas_profiling.ProfileReport(df, title='Data Audit Report \nAuthor: Ved')
+    profile.to_file(output_file="../reports/data_audit_report.html")
+    
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("../input/train.csv")
+
+    create_summary_report(df)
