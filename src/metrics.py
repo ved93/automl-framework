3c3620d9a920610a9e518dabe8ebbfb6494e4389
ved93 <vpved93@gmail.com>
2020-06-15 20:18:37 +0530

    Added : Regression metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,57 @@
 from sklearn import metrics as skmetrics
+import numpy as np
+
+
+class RegressionMetrics:
+    def __init__(self):
+        self.metrics = {
+            "mae": self._mae,
+            "mse": self._mse,
+            "rmse": self._rmse,
+            "msle": self._msle,
+            "rmsle": self._rmsle,
+            "r2": self._r2,
+        }
+
+    def __call__(self, metric, y_true, y_pred):
+
+        if metric not in self.metrics:
+            raise Exception("Metric not implemented")
+        if metric == "mae":
+            return self._mae(y_true, y_pred)
+        elif metric == "mse":
+            return self._mse(y_true, y_pred)
+        elif metric == "rmse":
+            return self._rmse(y_true, y_pred)
+        elif metric == "msle":
+            return self._msle(y_true, y_pred)
+        elif metric == "rmsle":
+            return self._rmsle(y_true, y_pred)
+        elif metric == "r2":
+            return self._r2(y_true, y_pred)
+
+    @staticmethod
+    def _mae(y_true, y_pred):
+        return skmetrics.mean_absolute_error(y_true, y_pred)
+
+    @staticmethod
+    def _mse(y_true, y_pred):
+        return skmetrics.mean_squared_error(y_true, y_pred)
+
+    def _rmse(self, y_true, y_pred):
+        return np.sqrt(self._mse(y_true, y_pred))
+
+    @staticmethod
+    def _msle(y_true, y_pred):
+        return skmetrics.mean_squared_log_error(y_true, y_pred)
+
+    def _rmsle(self, y_true, y_pred):
+        return np.sqrt(self._msle(y_true, y_pred))
+
+    @staticmethod
+    def _r2(y_true, y_pred):
+        return skmetrics.r2_score(y_true, y_pred)
+
 
 class ClassificationMetrics:
     def __init__(self):
@@ -8,9 +61,9 @@ class ClassificationMetrics:
             "precision": self._precision,
             "recall": self._recall,
             "auc": self._auc,
-            "logloss": self._logloss
+            "logloss": self._logloss,
         }
-    
+
     def __call__(self, metric, y_true, y_pred, y_proba=None):
         if metric not in self.metrics:
             raise Exception("Metric not implemented")
@@ -34,19 +87,19 @@ class ClassificationMetrics:
     @staticmethod
     def _accuracy(y_true, y_pred):
         return skmetrics.accuracy_score(y_true=y_true, y_pred=y_pred)
-    
+
     @staticmethod
     def _f1(y_true, y_pred):
         return skmetrics.f1_score(y_true=y_true, y_pred=y_pred)
-    
+
     @staticmethod
     def _recall(y_true, y_pred):
         return skmetrics.recall_score(y_true=y_true, y_pred=y_pred)
-    
+
     @staticmethod
     def _precision(y_true, y_pred):
         return skmetrics.precision_score(y_true=y_true, y_pred=y_pred)
-    
+
     @staticmethod
     def _logloss(y_true, y_pred):
         return skmetrics.log_loss(y_true=y_true, y_pred=y_pred)
